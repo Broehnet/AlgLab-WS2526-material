a41401a0ae2416aa432fca7243eb7e330bbc41c7
Bennet Stanitzek <b.stanitzek@tu-bs.de>
2025-10-29 16:45:40 +0100

    Aufgabe 1A

--- sheets/01_cpsat/exercises/00_foobar/solution_cpsat.py
+++ sheets/01_cpsat/exercises/00_foobar/solution_cpsat.py
@@ -8,8 +8,24 @@ def solve(instance: Instance) -> Solution:
     """
     numbers = instance.numbers
     model = cp_model.CpModel()
+    domain1 = cp_model.Domain.from_values(numbers)
+    x1 = model.new_int_var_from_domain(domain1, "x1")
+    x2 = model.new_int_var_from_domain(domain1, "x2")
+
+    #Variante 1
+    absx1x2 = model.new_int_var(0, abs(max(numbers, key=abs))*2, "absx1x2")
+    model.add_abs_equality(target=absx1x2, expr=x2-x1)
+    model.maximize(absx1x2)
+
+    #Variante  2
+    # model.add(x2 >= x1)
+    # model.maximize(x2 - x1)
+
+    solver = cp_model.CpSolver()
+    solver.solve(model)
     return Solution(
-        number_a=numbers[0],
-        number_b=numbers[-1],
-        distance=abs(numbers[0] - numbers[-1]),
+        number_a=solver.Value(x1),
+        number_b=solver.Value(x2),
+        # distance=solver.Value(x2) - solver.Value(x1),
+        distance=solver.Value(absx1x2),
     )
--- sheets/01_cpsat/exercises/00_foobar/solution_python.py
+++ sheets/01_cpsat/exercises/00_foobar/solution_python.py
@@ -6,8 +6,15 @@ def solve(instance: Instance) -> Solution:
     Implement your solver for the problem here!
     """
     numbers = instance.numbers
+    smallest = numbers[0]
+    biggest = numbers[0]
+    for number in numbers:
+        if number < smallest:
+            smallest = number
+        if number > biggest:
+            biggest = number
     return Solution(
-        number_a=numbers[0],
-        number_b=numbers[-1],
-        distance=abs(numbers[0] - numbers[-1]),
+        number_a=smallest,
+        number_b=biggest,
+        distance=abs(smallest - biggest),
     )
